bcd5fa9fe7826b9f47c569d877ac74a8d3ac744d
Vahagn <vahagn.miqayelyan.90@gmail.com>
2026-03-14 14:20:29 +0400

    feat: added pm2 config

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [{
+        name: "open-v-robotics-system",
+        cwd: 'open-v-robotics-system/server',
+        script: "./index.js",
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: "production",
+        },
+        ulimit: 0
+    }]
+};
